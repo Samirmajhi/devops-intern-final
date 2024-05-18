002fff10e80924a6e7b556a695b23f34744bc397
samir-devops <samirmajhi@gmail.com>
2024-05-19 00:34:01 +0530

    yes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World guys this is beautiful Let it happen!');
+  res.send('Hello, World !');
 });
 
 app.listen(port, () => {
